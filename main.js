e50f7fa4dd717370351de7f64e49596ffb62eaae
fuego44 <victorandreum@gmail.com>
2024-04-18 20:32:21 +0200

    finis

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const range = document.querySelector("#range")
+const dinero = document.getElementById("dinero")
+const pageViews = document.getElementById("pageV")
+const btn = document.getElementById("btn-div")
+
+btn.addEventListener("click", function(){
+    btn.classList.toggle("active")
+    checkNumber()
+})
 
 range.addEventListener("input", () => {
 
@@ -7,7 +15,31 @@ range.addEventListener("input", () => {
     const currentVal = range.value
 
     range.style.backgroundSize = ((currentVal - min) / (max - min)) * 100 + "% 100%"
+
+
+    checkNumber()
+
+
 } )
+
+function checkNumber(){
+    let dineroNumber = Number(dinero.textContent)
+    let pageValue = pageViews.textContent
+
+    if(range.value == 0){dineroNumber = 8; pageValue = "10K"} 
+    else if(range.value == 25) {dineroNumber = 12; pageValue = "50K"}
+    else if(range.value == 50) {dineroNumber = 16; pageValue = "100K"}
+    else if(range.value == 75) {dineroNumber = 24; pageValue = "500K"}
+    else {dineroNumber = 36; pageValue = "1M"}
+
+    if(btn.classList.contains("active")){
+        dineroNumber = dineroNumber * 0.75
+    }
+
+    dinero.textContent = dineroNumber.toFixed(2);
+    pageViews.textContent = pageValue
+}
+
 
 /* 
 
